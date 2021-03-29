c0751033981298eca9a4cc9a9dae61fd0988583e
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-03-29 15:36:42 +0500

    Register login complte

---
+++ RegisterLogin/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegisterLogin.Models;
+
+namespace RegisterLogin.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        awamEntities db = new awamEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(the_user t)
+        {
+            db.the_user.Add(t);
+            db.SaveChanges();
+            ModelState.Clear();
+            return RedirectToAction("login","Home");
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(the_user e)
+        {
+          var f=  db.the_user.Where(a => a.email == e.email && a.password == e.password).FirstOrDefault();
+            if (f!=null)
+            {
+                return RedirectToAction("aptech", "Home");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+        public ActionResult aptech()
+        {
+            return View();
+        }
+    }
+}
